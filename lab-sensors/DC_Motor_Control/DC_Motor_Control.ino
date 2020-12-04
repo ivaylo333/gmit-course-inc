20fd1499edc14c21e1da77243f0829bda54389c9
ivaylo333 <ivaylo.dimitrov.id@gmail.com>
2020-12-04 12:02:18 +0000

    button

--- lab-sensors/DC_Motor_Control/DC_Motor_Control.ino
+++ lab-sensors/DC_Motor_Control/DC_Motor_Control.ino
@@ -1,27 +1,51 @@
 
-int MOT = 9;           // motor output pin
-int motor_speed;    // motor speed value
-int delay_time ;
-int sensorPin = A0;    // select the input pin for the potentiometer
+*
+  Smoothing
+
+
+
+
+// Define the number of samples to keep track of. The higher the number, the
+// more the readings will be smoothed, but the slower the output will respond to
+// the input. Using a constant rather than a normal variable lets us use this
+// value to determine the size of the readings array.
+const int numReadings = 10;
+
+int readings[numReadings];      // the readings from the analog input
+int readIndex = 0;              // the index of the current reading
+int total = 0;                  // the running total
+int average = 0;                // the average
+
+int inputPin = A0;
 
 void setup() {
+  // initialize serial communication with computer:
   Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(MOT, OUTPUT);
+  // initialize all the readings to 0:
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
 }
 
 void loop() {
+  // subtract the last reading:
+  total = total - readings[readIndex];
+  // read from the sensor:
+  readings[readIndex] = analogRead(inputPin);
+  // add the reading to the total:
+  total = total + readings[readIndex];
+  // advance to the next position in the array:
+  readIndex = readIndex + 1;
 
-// read the value from the sensor:
- delay_time = analogRead(sensorPin);
+  // if we're at the end of the array...
+  if (readIndex >= numReadings) {
+    // ...wrap around to the beginning:
+    readIndex = 0;
+  }
 
-analogWrite(MOT, motor_speed);
-  
-  motor_speed = 255-delay_time;
-  Serial.print("Motor Speed is:");
-  Serial.println(motor_speed);
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(delay_time);                       // wait for time set by potentiometer
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(delay_time);                       // wait for time set by potentiometer
+  // calculate the average:
+  average = total / numReadings;
+  // send it to the computer as ASCII digits
+  Serial.println(average);
+  delay(1);        // delay in between reads for stability
 }
